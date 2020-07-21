19ca8657fee01352d6a470c1b9d2ba326d4ff5de
Florian Forster <octo@collectd.org>
2020-07-21 22:34:56 +0200

    Initial import of the collectd website.

--- main.go
+++ main.go
@@ -16,7 +16,19 @@ var templates *template.Template
 
 func main() {
 	var err error
-	templates, err = template.ParseFiles("templates/index.html")
+	templates, err = template.ParseFiles(
+		"templates/index.shtml",
+		"templates/header.shtml",
+		"templates/menu.shtml",
+		"templates/footer.shtml",
+		"templates/contact.shtml",
+		"templates/dev-info.shtml",
+		"templates/documentation.shtml",
+		"templates/download.shtml",
+		"templates/faq.shtml",
+		"templates/features.shtml",
+		"templates/related.shtml",
+	)
 	if err != nil {
 		log.Fatalln("template.ParseFiles:", err)
 	}
@@ -26,7 +38,7 @@ func main() {
 		ProjectID: os.Getenv("GOOGLE_CLOUD_PROJECT"),
 	})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 	trace.RegisterExporter(exporter)
 	trace.ApplyConfig(trace.Config{
@@ -39,18 +51,39 @@ func main() {
 		log.Printf("Defaulting to port %s", port)
 	}
 
-	http.Handle("/", &ochttp.Handler{
-		Propagation:      &propagation.HTTPFormat{},
-		Handler:          http.HandlerFunc(handler),
-		IsPublicEndpoint: true,
-	})
+	handlers := []struct {
+		Prefix, Template, Title string
+	}{
+		{"/", "index.shtml", "Start page"},
+		{"/index.shtml", "index.shtml", "Start page"},
+		{"/contact.shtml", "contact.shtml", "Contact"},
+		{"/dev-info.shtml", "dev-info.shtml", "Development Information"},
+		{"/documentation.shtml", "documentation.shtml", "Documentation"},
+		{"/download.shtml", "download.shtml", "Download"},
+		{"/faq.shtml", "faq.shtml", "Frequently asked questions"},
+		{"/features.shtml", "features.shtml", "Features"},
+		{"/related.shtml", "related.shtml", "Related sites"},
+	}
+	for _, hndl := range handlers {
+		http.Handle(hndl.Prefix, &ochttp.Handler{
+			Propagation:      &propagation.HTTPFormat{},
+			Handler:          templateHandler{hndl.Template, hndl.Title},
+			IsPublicEndpoint: true,
+		})
+	}
+
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatalln("http.ListenAndServe:", err)
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	if err := templates.Execute(w, r); err != nil {
+type templateHandler struct {
+	Name  string
+	Title string
+}
+
+func (h templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := templates.ExecuteTemplate(w, h.Name, h); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
